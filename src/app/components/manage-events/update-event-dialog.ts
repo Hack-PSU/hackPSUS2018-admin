f081bf8a649a68e78448aef4a39151fba7757153
Sushrut Shringarputale <sush.shring@gmail.com>
2018-10-05 14:43:11 -0400

    New updates to admin app (#40)
    * Added the ability to buffer outgoing emails, and some minor fixes (#36)
    
    Signed-off-by: Sushrut Shringarputale <contact@sushshring.com>
    
    * Fixed locations, user count, checkin, and live updates (#37)
    
    * Fixed Locations, User Count, and Checkin
    
    * Live updates work now
    
    * Address PR comments
    
    Signed-off-by: Sushrut Shringarputale <contact@sushshring.com>
    
    * Fixed event updates (#39)

--- src/app/components/manage-events/update-event-dialog.ts
+++ src/app/components/manage-events/update-event-dialog.ts
@@ -1,10 +1,10 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { EventModel } from '../../models/event-model';
 import { HttpAdminService } from '../../services/services';
 import { LocationModel } from '../../models/location-model';
-import { Observable } from 'rxjs';
-import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-update-event-dialog',
@@ -21,8 +21,8 @@ export class UpdateEventDialogComponent {
     private httpService: HttpAdminService,
     @Inject(MAT_DIALOG_DATA) public data: any,
   ) {
-    if (data.event_start_time) { data.event_start_time = new Date(data.event_start_time * 1000); }
-    if (data.event_end_time) { data.event_end_time = new Date(data.event_end_time * 1000); }
+    if (data.event_start_time) { data.event_start_time = new Date(parseInt(data.event_start_time, 10)); }
+    if (data.event_end_time) { data.event_end_time = new Date(parseInt(data.event_end_time, 10)); }
     this.outData = data;
     this.locationList = this.httpService.getLocations()
                             .pipe(
--- src/app/services/http-admin/http-admin.service.ts
+++ src/app/services/http-admin/http-admin.service.ts
@@ -58,7 +58,10 @@ export class HttpAdminService extends BaseHttpService {
       true,
       limit ? new Map<string, any>().set('limit', limit) : null,
     );
-    return super.genericGet<EventModel[]>(apiRoute);
+    return super.genericGet<{}[]>(apiRoute)
+      .pipe(
+        map(events => events.map(event => EventModel.parseJSON(event))),
+      )
   }
 
   addEvent(event: EventModel): Observable<{}> {
@@ -74,7 +77,7 @@ export class HttpAdminService extends BaseHttpService {
     'live/event',
     true,
     );
-    return super.genericPut<{}>(apiRoute, event.restRepr());
+    return super.genericPut<{}>(apiRoute, { event: event.restRepr() });
   }
   
   getUserUID(email: string) {
